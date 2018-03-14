6c8a9b17d5b6829685b8177874550b333b5bff71
Tiago <vrealtiago@gmail.com>
2018-03-14 10:57:29 +0100

    jsES6 - lesson 04

--- jsTraining/jsES6/JSA1/aluraframe/TestCode/Test.js
+++ jsTraining/jsES6/JSA1/aluraframe/TestCode/Test.js
@@ -1,11 +1,21 @@
-// let numeros = [3,2,11,20,8,7];
-// let numerosC = numeros.map(item => item % 2 ? item * 2 : item);
-// console.log(numerosC);
-// function somaDoisNumeros(numero1, numero2) {
-//     return numero1 + numero2;
-// }
-// let numeros = [10, 30];
-// console.log(somaDoisNumeros(...numeros));
+/*
+================================================================
+*/
+
+/*let numeros = [3,2,11,20,8,7];
+let numerosC = numeros.map(item => item % 2 ? item * 2 : item);
+console.log(numerosC);
+function somaDoisNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+let numeros = [10, 30];
+console.log(somaDoisNumeros(...numeros));*/
+
+/*
+================================================================
+*/
+
+/*
 class Aluno {
 
     constructor(matricula, nome) {
@@ -38,4 +48,97 @@ var aprovados = avaliacoes
     .filter(prova => prova.nota >= 7)
     .map(prova => prova.aluno.nome);
 
-console.log(aprovados);
+console.log(aprovados);*/
+
+/*
+================================================================
+*/
+
+   /* function validaCodigo(codigo) {
+
+        // cria a expressão regular. Poderíamos ter usado
+        // a sintaxe new RegExp(/\D{3}-\D{2}-\d{2}/)
+        // \D é qualquer coisa não dígito, \D{3} é qualquer coisa não dígito que forme um grupo de 3 caracteres. E \d é qualquer dígito.
+        let expressao = /\D{3}-\D{2}-\d{2}/;
+
+        // toda expressão regular possui o método test
+        // que recebe o alvo do teste, retornando true
+        // se passou, e false se falhou
+        if(expressao.test(codigo)) {
+            alert('Código válido!');
+        } else {
+            alert('Código inválido');
+        }
+
+    }
+
+    validaCodigo('GWZ-JJ-12'); // válido
+    validaCodigo('1X1-JJ-12'); // inválido
+
+class Codigo{
+
+    constructor(texto) {
+        if(!this._valida(texto)) throw new Error(`O texto ${texto} não é um código válido`);
+        this._texto = texto;
+    }
+
+    _valida(texto) {
+        return /\D{3}-\D{2}-\d{2}/.test(texto);
+    }
+    get texto() {
+        return this._texto;
+    }
+}
+
+
+let codigo1 = new Codigo('GWZ-JJ-12'); // válido
+console.log(codigo1.texto);
+let codigo2 = new Codigo('1X1-JJ-12'); // inválido
+console.log(codigo2.texto);
+
+class Codigo{
+
+    constructor(expressao){
+        this._expressao = expressao;
+    }
+    validaCodigo(codigo){
+        let code = this._expressao.test(codigo) ? 'codigo valido' : 'codigo invalido';
+        return code;
+    }
+}
+
+
+let code1 = new Codigo(/\D{3}-\D{2}-\d{2}/);
+console.log(code1.validaCodigo('GWZ-JJ-12'));
+console.log(code1.validaCodigo('1X1-JJ-12'));
+*/
+
+
+/*
+================================================================
+*/
+
+/*function exibeNoConsole(lista) {
+    lista.forEach(item => console.log(item));
+}
+
+let listaDeNomes1 = ['Flávio', 'Rogers', 'Júlia'];
+// exibeNoConsole(listaDeNomes1);
+let listaDeNomes2 = ['Vieira', 'Fernanda', 'Gerson'];
+// exibeNoConsole(listaDeNomes2);
+exibeNoConsole([].concat(listaDeNomes1,listaDeNomes2));
+*/
+
+/*
+================================================================
+*/
+
+class ConversorXML {
+
+    static converte(objeto) {
+        // converte um objeto em XML
+    }
+}
+
+let conversor = new ConversorXML();
+conversor.converte({nome: 'Guaraci', idade: 40});
--- jsTraining/jsES6/JSA1/aluraframe/client/js/app/controller/NegociacaoController.js
+++ jsTraining/jsES6/JSA1/aluraframe/client/js/app/controller/NegociacaoController.js
@@ -5,30 +5,28 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listanegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value,
-        );
-        console.log(negociacao)
-        this.fieldClean();
+        this._listanegociacoes.adiciona(this._criaNegociacao());
+        this._cleanForm();
     }
 
-    fieldClean(){
-        let $ = document.querySelector.bind(document);
-        $('#data').value = '';
-        $('#quantidade').value = 1;
-        $('#valor').value = 0;
-        $('#data').focus();
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _cleanForm(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 
 }
